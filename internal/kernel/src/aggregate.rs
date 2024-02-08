e497bbea5f21739ed5d7ddc70acf85350326ee1b
pyama2000 <takahiko-yamashita@retty.me>
2024-02-08 23:07:11 +0900

    add: 部品の集約を表す構造体 (WidgetAggregate) を追加する

---
+++ internal/kernel/src/aggregate.rs
@@ -0,0 +1,32 @@
+use crate::Id;
+
+/// 部品 (Widget) の集約
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct WidgetAggregate {
+    id: Id<WidgetAggregate>,
+    name: String,
+    description: String,
+    version: u64,
+}
+
+impl WidgetAggregate {
+    /// 部品の id
+    pub fn id(&self) -> &Id<WidgetAggregate> {
+        &self.id
+    }
+
+    /// 部品の名前
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    /// 部品の説明
+    pub fn description(&self) -> &str {
+        &self.description
+    }
+
+    /// 集約のバージョン (= 更新回数)
+    pub fn version(&self) -> u64 {
+        self.version
+    }
+}
--- internal/kernel/src/lib.rs
+++ internal/kernel/src/lib.rs
@@ -2,6 +2,8 @@ use std::marker::PhantomData;
 
 use ulid::Ulid;
 
+pub mod aggregate;
+
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Id<T> {
     value: Ulid,
