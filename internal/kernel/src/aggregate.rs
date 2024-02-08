e4540cb54678b7a1a0955314615c93254c7f3274
pyama2000 <takahiko-yamashita@retty.me>
2024-02-08 23:42:59 +0900

    add: 集約に対するコマンドの処理を成功して保存可能になった状態を表す構造体 (WidgetCommandState) を追加する

--- internal/kernel/src/aggregate.rs
+++ internal/kernel/src/aggregate.rs
@@ -1,3 +1,4 @@
+use crate::event::WidgetEvent;
 use crate::Id;
 
 /// 部品 (Widget) の集約
@@ -28,5 +29,30 @@ impl WidgetAggregate {
     /// 集約のバージョン (= 更新回数)
     pub fn version(&self) -> u64 {
         self.version
+    }
+}
+
+/// 集約 (Aggregate) に対するコマンドの処理を成功して保存可能になった状態
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct WidgetCommandState {
+    widget_id: Id<WidgetAggregate>,
+    events: Vec<WidgetEvent>,
+    aggregate_version: u64,
+}
+
+impl WidgetCommandState {
+    /// 部品の id
+    pub fn widget_id(&self) -> &Id<WidgetAggregate> {
+        &self.widget_id
+    }
+
+    /// 部品に対するコマンド
+    pub fn events(&self) -> &[WidgetEvent] {
+        &self.events
+    }
+
+    /// 集約のバージョン
+    pub fn aggregate_version(&self) -> u64 {
+        self.aggregate_version
     }
 }
