6eff69db0a7f319c62d14c07c8765994948c2f08
pyama2000 <takahiko-yamashita@retty.me>
2024-02-08 23:34:33 +0900

    add: 部品に発生するイベントの列挙体 (WidgetEvent) を追加する

---
+++ internal/kernel/src/event.rs
@@ -0,0 +1,29 @@
+use crate::Id;
+
+/// 部品 (Widget) に発生するイベント
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum WidgetEvent {
+    /// 部品を新しく作成する
+    WidgetCreated {
+        /// イベントの id
+        id: Id<WidgetEvent>,
+        /// 部品の名前
+        widget_name: String,
+        /// 部品の説明
+        widget_description: String,
+    },
+    /// 部品の名前を変更する
+    WidgetNameChanged {
+        /// イベントの id
+        id: Id<WidgetEvent>,
+        /// 部品の名前
+        widget_name: String,
+    },
+    /// 部品の説明を変更する
+    WidgetDescriptionChanged {
+        /// イベントの id
+        id: Id<WidgetEvent>,
+        /// 部品の名前
+        description: String,
+    },
+}
--- internal/kernel/src/lib.rs
+++ internal/kernel/src/lib.rs
@@ -4,6 +4,7 @@ use ulid::Ulid;
 
 pub mod aggregate;
 pub mod command;
+pub mod event;
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Id<T> {
