b9677783d00e77b0f2f0bff87508107e59452ac5
pyama2000 <takahiko-yamashita@retty.me>
2024-02-09 00:01:29 +0900

    add: 独自の Result 型を定義する

--- internal/lib/src/lib.rs
+++ internal/lib/src/lib.rs
@@ -1,14 +1,1 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub type Result<T> = core::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
