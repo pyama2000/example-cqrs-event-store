e9b17eb16c072693f03ea608d9bfd9859c7d6c3c
pyama2000 <takahiko-yamashita@retty.me>
2024-02-08 23:35:04 +0900

    feat: コマンドとイベントを紐づける

--- internal/kernel/src/command.rs
+++ internal/kernel/src/command.rs
@@ -1,10 +1,12 @@
+use crate::event::WidgetEvent;
+
 /// 部品 (Widget) に対するコマンド
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum WidgetCommand {
     /// 部品を新しく作成する
-    CreateWidget,
+    CreateWidget(WidgetEvent),
     /// 部品の名前を変更する
-    ChangeWidgetName,
+    ChangeWidgetName(WidgetEvent),
     /// 部品の説明を変更する
-    ChangeWidgetDescription,
+    ChangeWidgetDescription(WidgetEvent),
 }
