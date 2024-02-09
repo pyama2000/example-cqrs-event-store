f8b3019f6f2520b916caf2b14e3bb015142887a1
pyama2000 <takahiko-yamashita@retty.me>
2024-02-10 01:29:21 +0900

    feat: Aggregate に対するコマンドの実行を WidgetCommandExecutor に委譲する

--- internal/kernel/src/aggregate.rs
+++ internal/kernel/src/aggregate.rs
@@ -1,7 +1,7 @@
 use lib::Result;
 
 use crate::command::WidgetCommand;
-use crate::error::AggregateError;
+use crate::error::CommandError;
 use crate::event::WidgetEvent;
 use crate::Id;
 
@@ -37,19 +37,9 @@ impl WidgetAggregate {
 
     /// 集約にコマンドを実行する
     pub fn apply_command(self, command: WidgetCommand) -> Result<WidgetCommandState> {
-        let events = match command {
-            WidgetCommand::CreateWidget(event) => vec![event],
-            WidgetCommand::ChangeWidgetName(event) => vec![event],
-            WidgetCommand::ChangeWidgetDescription(event) => vec![event],
-        };
-        Ok(WidgetCommandState {
-            widget_id: self.id,
-            events,
-            aggregate_version: self
-                .version
-                .checked_add(1)
-                .ok_or(AggregateError::VersionUpdateLimitReached)?,
-        })
+        WidgetCommandExecutor::new(self, command)
+            .validate()
+            .execute()
     }
 }
 
@@ -75,5 +65,135 @@ impl WidgetCommandState {
     /// 集約のバージョン
     pub fn aggregate_version(&self) -> u64 {
         self.aggregate_version
+    }
+}
+
+/// 集約 (Aggregate) に対する `WidgetCommand` が有効か確認して `WidgetCommandState` を作成するビルダー
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct WidgetCommandExecutor<IsEventsValid, IsNameValid, IsDescriptionValid> {
+    aggregate: WidgetAggregate,
+    command: WidgetCommand,
+    is_events_valid: IsEventsValid,
+    is_widget_name_valid: IsNameValid,
+    is_widget_description_valid: IsDescriptionValid,
+}
+
+impl WidgetCommandExecutor<(), (), ()> {
+    fn new(aggregate: WidgetAggregate, command: WidgetCommand) -> Self {
+        Self {
+            aggregate,
+            command,
+            is_events_valid: (),
+            is_widget_name_valid: (),
+            is_widget_description_valid: (),
+        }
+    }
+
+    /// Aggregate に対するコマンドが有効か確認する
+    fn validate(self) -> WidgetCommandExecutor<bool, bool, bool> {
+        self.validate_events()
+            .validate_widget_name()
+            .validate_widget_description()
+    }
+}
+
+impl WidgetCommandExecutor<(), (), ()> {
+    /// コマンドに含まれるイベントが有効か確認する
+    fn validate_events(self) -> WidgetCommandExecutor<bool, (), ()> {
+        let is_events_valid = match &self.command {
+            WidgetCommand::CreateWidget(event) => {
+                matches!(event, WidgetEvent::WidgetCreated { .. })
+            }
+            WidgetCommand::ChangeWidgetName(event) => {
+                matches!(event, WidgetEvent::WidgetNameChanged { .. })
+            }
+            WidgetCommand::ChangeWidgetDescription(event) => {
+                matches!(event, WidgetEvent::WidgetDescriptionChanged { .. })
+            }
+        };
+        WidgetCommandExecutor {
+            aggregate: self.aggregate,
+            command: self.command,
+            is_events_valid,
+            is_widget_name_valid: self.is_widget_name_valid,
+            is_widget_description_valid: self.is_widget_description_valid,
+        }
+    }
+}
+
+impl<IsNameValid, IsDescriptionValid> WidgetCommandExecutor<bool, IsNameValid, IsDescriptionValid> {
+    /// イベントの部品の名前が有効か確認する
+    fn validate_widget_name(self) -> WidgetCommandExecutor<bool, bool, IsDescriptionValid> {
+        let is_widget_name_valid = match &self.command {
+            WidgetCommand::CreateWidget(event) | WidgetCommand::ChangeWidgetName(event) => {
+                match event {
+                    WidgetEvent::WidgetCreated { widget_name, .. }
+                    | WidgetEvent::WidgetNameChanged { widget_name, .. } => !widget_name.is_empty(),
+                    WidgetEvent::WidgetDescriptionChanged { .. } => true,
+                }
+            }
+            WidgetCommand::ChangeWidgetDescription(_) => true,
+        };
+        WidgetCommandExecutor {
+            aggregate: self.aggregate,
+            command: self.command,
+            is_events_valid: self.is_events_valid,
+            is_widget_name_valid,
+            is_widget_description_valid: self.is_widget_description_valid,
+        }
+    }
+
+    /// イベントの部品の説明が有効か確認する
+    fn validate_widget_description(self) -> WidgetCommandExecutor<bool, IsNameValid, bool> {
+        let is_widget_description_valid = match &self.command {
+            WidgetCommand::CreateWidget(event) | WidgetCommand::ChangeWidgetDescription(event) => {
+                match event {
+                    WidgetEvent::WidgetCreated {
+                        widget_description, ..
+                    }
+                    | WidgetEvent::WidgetDescriptionChanged {
+                        widget_description, ..
+                    } => !widget_description.is_empty(),
+                    WidgetEvent::WidgetNameChanged { .. } => true,
+                }
+            }
+            WidgetCommand::ChangeWidgetName(_) => true,
+        };
+        WidgetCommandExecutor {
+            aggregate: self.aggregate,
+            command: self.command,
+            is_events_valid: self.is_events_valid,
+            is_widget_name_valid: self.is_widget_name_valid,
+            is_widget_description_valid,
+        }
+    }
+}
+
+impl WidgetCommandExecutor<bool, bool, bool> {
+    /// コマンドの実行結果を返す
+    fn execute(self) -> Result<WidgetCommandState> {
+        if !self.is_events_valid {
+            return Err(Box::new(CommandError::InvalidEvent));
+        }
+        if !self.is_widget_name_valid {
+            return Err(Box::new(CommandError::InvalidWidgetName));
+        }
+        if !self.is_widget_description_valid {
+            return Err(Box::new(CommandError::InvalidWidgetDescription));
+        }
+        let events = match self.command {
+            WidgetCommand::CreateWidget(event)
+            | WidgetCommand::ChangeWidgetName(event)
+            | WidgetCommand::ChangeWidgetDescription(event) => vec![event],
+        };
+        Ok(WidgetCommandState {
+            widget_id: self.aggregate.id,
+            events,
+            aggregate_version: self
+                .aggregate
+                .version
+                .checked_add(1)
+                .ok_or(Box::new(CommandError::VersionUpdateLimitReached))?,
+        })
     }
 }
--- internal/kernel/src/error.rs
+++ internal/kernel/src/error.rs
@@ -1,7 +1,18 @@
 use thiserror::Error;
 
+/// コマンドの実行に失敗したことを表す
 #[derive(Error, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub enum AggregateError {
+pub enum CommandError {
+    /// Aggregate のバージョンが上限を超えたときのエラー
     #[error("Cannot update Aggregate version")]
     VersionUpdateLimitReached,
+    /// コマンドに不正なイベントが含まれるときのエラー
+    #[error("Invalid event found")]
+    InvalidEvent,
+    /// イベントに含まれる部品の名前が不正なフォーマットのときのエラー
+    #[error("Invalid name for the widget")]
+    InvalidWidgetName,
+    /// イベントに含まれる部品の説明が不正なフォーマットのときのエラー
+    #[error("Invalid description for the widget")]
+    InvalidWidgetDescription,
 }
