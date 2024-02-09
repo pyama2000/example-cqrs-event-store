e3f9f946f258c0f40e85ce021151f6cb9bd8d6f1
T.YAMASHITA <33086493+pyama2000@users.noreply.github.com>
2024-02-10 00:57:16 +0900

    add: 集約を永続化する処理のインターフェイスを追加する (#2)
    chore(doc): CommandProcessor のドキュメンテーションコメントを修正する

---
+++ internal/kernel/src/processor.rs
@@ -0,0 +1,23 @@
+use lib::Result;
+
+use crate::aggregate::{WidgetAggregate, WidgetCommandState};
+use crate::Id;
+
+/// 集約を永続化する処理のインターフェイス
+pub trait CommandProcessor {
+    /// 部品の集約を新しく作成する
+    fn create_widget_aggregate(
+        &self,
+        command_state: WidgetCommandState,
+    ) -> impl std::future::Future<Output = Result<()>> + Send;
+    /// 部品の集約を取得する
+    fn get_widget_aggregate(
+        &self,
+        widget_id: Id<WidgetAggregate>,
+    ) -> impl std::future::Future<Output = Result<WidgetAggregate>> + Send;
+    /// 部品の集約を更新する
+    fn update_widget_aggregate(
+        &self,
+        command_state: WidgetCommandState,
+    ) -> impl std::future::Future<Output = Result<()>> + Send;
+}
