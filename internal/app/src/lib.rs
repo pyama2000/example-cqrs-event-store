a3dc210ebd47046c7e2ff84138ebedd526a8d88d
pyama2000 <takahiko-yamashita@retty.me>
2024-02-09 12:16:55 +0900

    add: 部品のユースケース処理のインターフェイスと実体を追加する

--- internal/app/src/lib.rs
+++ internal/app/src/lib.rs
@@ -1,14 +1,49 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::future::Future;
+
+use kernel::processor::CommandProcessor;
+use lib::Result;
+
+/// 部品 (Widget) のユースケース処理のインターフェイス
+pub trait WidgetService {
+    /// 部品を新しく作成する
+    fn create_widget(
+        &self,
+        widget_name: String,
+        widget_description: String,
+    ) -> impl Future<Output = Result<()>> + Send;
+    /// 部品の名前を変更する
+    fn change_widget_name(
+        &self,
+        widget_id: String,
+        widget_name: String,
+    ) -> impl Future<Output = Result<()>> + Send;
+    /// 部品の説明を変更する
+    fn change_widget_description(
+        &self,
+        widget_id: String,
+        widget_description: String,
+    ) -> impl Future<Output = Result<()>> + Send;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct WidgetServiceImpl<C: CommandProcessor> {
+    command: C,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<C: CommandProcessor + Send + Sync + 'static> WidgetService for WidgetServiceImpl<C> {
+    async fn create_widget(&self, _widget_name: String, _widget_description: String) -> Result<()> {
+        todo!()
+    }
+
+    async fn change_widget_name(&self, _widget_id: String, _widget_name: String) -> Result<()> {
+        todo!()
+    }
+
+    async fn change_widget_description(
+        &self,
+        _widget_id: String,
+        _widget_description: String,
+    ) -> Result<()> {
+        todo!()
     }
 }
