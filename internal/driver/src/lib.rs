8d8e39e9c8e1167f681f19beadf5b0d93b0f2ba5
pyama2000 <takahiko-yamashita@retty.me>
2024-02-11 10:20:26 +0900

    add: HTTP server を起動する Server 構造体を追加する

--- internal/driver/src/lib.rs
+++ internal/driver/src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use axum::http::StatusCode;
+use axum::routing::get;
+use axum::Router;
+use lib::Result;
+use tokio::net::{TcpListener, ToSocketAddrs};
+
+#[derive(Debug, Clone)]
+pub struct Server<T: ToSocketAddrs> {
+    addr: T,
+    router: Router,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<T: ToSocketAddrs> Server<T> {
+    pub fn new(addr: T) -> Self {
+        Self {
+            addr,
+            router: Router::new(),
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub async fn run(self) -> Result<()> {
+        let app = self
+            .router
+            .route("/healthz", get(|| async { StatusCode::OK }));
+        let listener = TcpListener::bind(&self.addr).await?;
+        axum::serve(listener, app).await?;
+        Ok(())
     }
 }
