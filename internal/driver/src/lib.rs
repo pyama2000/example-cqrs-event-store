0e5a5222428aac626192f9ff9223d97d036512eb
pyama2000 <takahiko-yamashita@retty.me>
2024-02-11 10:21:12 +0900

    feat: graceful shutdown の処理を追加する

--- internal/driver/src/lib.rs
+++ internal/driver/src/lib.rs
@@ -3,6 +3,7 @@ use axum::routing::get;
 use axum::Router;
 use lib::Result;
 use tokio::net::{TcpListener, ToSocketAddrs};
+use tokio::signal;
 
 #[derive(Debug, Clone)]
 pub struct Server<T: ToSocketAddrs> {
@@ -23,7 +24,28 @@ impl<T: ToSocketAddrs> Server<T> {
             .router
             .route("/healthz", get(|| async { StatusCode::OK }));
         let listener = TcpListener::bind(&self.addr).await?;
-        axum::serve(listener, app).await?;
+        axum::serve(listener, app)
+            .with_graceful_shutdown(shutdown_signal())
+            .await?;
         Ok(())
     }
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .unwrap_or_else(|e| panic!("failed to install Ctrl+C handler: {e}"))
+    };
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .unwrap_or_else(|e| panic!("failed to install signal handler: {e}"))
+            .recv()
+            .await;
+    };
+    tokio::select! {
+        _ = ctrl_c => println!("receive ctrl_c signal"),
+        _ = terminate => println!("receive terminate"),
+    }
+    println!("signal received, starting graceful shutdown");
 }
