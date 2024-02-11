f28a7f2de65f08fa8ae05a653b15f0e4a426fd31
pyama2000 <takahiko-yamashita@retty.me>
2024-02-11 12:07:42 +0900

    feat: Request から widget_id や widget_name、widget_description を抜き出す

--- internal/driver/src/lib.rs
+++ internal/driver/src/lib.rs
@@ -1,11 +1,12 @@
 use std::sync::Arc;
 
 use app::WidgetService;
-use axum::extract::State;
+use axum::extract::{Path, State};
 use axum::http::StatusCode;
 use axum::routing::{get, post};
 use axum::{Json, Router};
 use lib::Error;
+use serde::Deserialize;
 use tokio::net::{TcpListener, ToSocketAddrs};
 use tokio::signal;
 
@@ -23,41 +24,78 @@ impl<T: ToSocketAddrs, S: WidgetService + Send + Sync + 'static> Server<T, S> {
     pub async fn run(self) -> Result<(), Error> {
         let router = Router::new()
             .route("/healthz", get(|| async { StatusCode::OK }))
-            .route(
-                "/create",
-                post(|State(service): State<Arc<S>>| async move {
-                    match service.create_widget(String::new(), String::new()).await {
-                        Ok(id) => Ok((
-                            StatusCode::CREATED,
-                            Json(serde_json::json!({ "widget_id": id })),
-                        )),
-                        Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string())),
-                    }
-                }),
-            )
-            .route(
-                "/name",
-                post(|State(service): State<Arc<S>>| async move {
-                    match service
-                        .change_widget_name(String::new(), String::new())
-                        .await
-                    {
-                        Ok(_) => Ok(StatusCode::ACCEPTED),
-                        Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string())),
-                    }
-                }),
-            )
-            .route(
-                "/description",
-                post(|State(service): State<Arc<S>>| async move {
-                    match service
-                        .change_widget_description(String::new(), String::new())
-                        .await
-                    {
-                        Ok(_) => Ok(StatusCode::ACCEPTED),
-                        Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string())),
-                    }
-                }),
+            .nest(
+                "/widgets",
+                Router::new()
+                    .route(
+                        "/",
+                        post(
+                            |State(service): State<Arc<S>>,
+                             Json(CreateWidget {
+                                 widget_name,
+                                 widget_description,
+                             }): Json<CreateWidget>| async move {
+                                match service.create_widget(widget_name, widget_description).await {
+                                    Ok(id) => Ok((
+                                        StatusCode::CREATED,
+                                        Json(serde_json::json!({ "widget_id": id })),
+                                    )),
+                                    Err(e) => {
+                                        Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
+                                    }
+                                }
+                            },
+                        ),
+                    )
+                    .nest(
+                        "/:widget_id",
+                        Router::new()
+                            .route(
+                                "/name",
+                                post(
+                                    |Path(widget_id): Path<String>,
+                                     State(service): State<Arc<S>>,
+                                     Json(ChangeWidgetName { widget_name }): Json<
+                                        ChangeWidgetName,
+                                    >| async move {
+                                        match service
+                                            .change_widget_name(widget_id, widget_name)
+                                            .await
+                                        {
+                                            Ok(_) => Ok(StatusCode::ACCEPTED),
+                                            Err(e) => Err((
+                                                StatusCode::INTERNAL_SERVER_ERROR,
+                                                e.to_string(),
+                                            )),
+                                        }
+                                    },
+                                ),
+                            )
+                            .route(
+                                "/description",
+                                post(
+                                    |Path(widget_id): Path<String>,
+                                     State(service): State<Arc<S>>,
+                                     Json(ChangeWidgetDescription { widget_description }): Json<
+                                        ChangeWidgetDescription,
+                                    >| async move {
+                                        match service
+                                            .change_widget_description(
+                                                widget_id,
+                                                widget_description,
+                                            )
+                                            .await
+                                        {
+                                            Ok(_) => Ok(StatusCode::ACCEPTED),
+                                            Err(e) => Err((
+                                                StatusCode::INTERNAL_SERVER_ERROR,
+                                                e.to_string(),
+                                            )),
+                                        }
+                                    },
+                                ),
+                            ),
+                    ),
             )
             .with_state(self.service);
         let listener = TcpListener::bind(&self.addr).await?;
@@ -66,6 +104,22 @@ impl<T: ToSocketAddrs, S: WidgetService + Send + Sync + 'static> Server<T, S> {
             .await?;
         Ok(())
     }
+}
+
+#[derive(Deserialize)]
+struct CreateWidget {
+    widget_name: String,
+    widget_description: String,
+}
+
+#[derive(Deserialize)]
+struct ChangeWidgetName {
+    widget_name: String,
+}
+
+#[derive(Deserialize)]
+struct ChangeWidgetDescription {
+    widget_description: String,
 }
 
 async fn shutdown_signal() {
