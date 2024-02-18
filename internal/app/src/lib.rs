7bc05c19a245d4b47b8c5a27099f1dc66b2db62a
pyama2000 <takahiko-yamashita@retty.me>
2024-02-18 22:49:58 +0900

    add: 部品名を変更するテストを追加する

--- internal/app/src/lib.rs
+++ internal/app/src/lib.rs
@@ -7,6 +7,8 @@ use kernel::processor::CommandProcessor;
 use lib::Error;
 use thiserror::Error;
 
+const MAX_RETRY_COUNT: u32 = 3;
+
 #[derive(Error, Debug)]
 pub enum WidgetServiceError {
     /// Aggregate が存在しないときのエラー
@@ -100,7 +102,6 @@ impl<C: CommandProcessor + Send + Sync + 'static> WidgetService for WidgetServic
         widget_id: String,
         widget_name: String,
     ) -> Result<(), WidgetServiceError> {
-        const MAX_RETRY_COUNT: u32 = 3;
         let mut retry_count = 0;
         loop {
             let aggregate = self
@@ -129,7 +130,6 @@ impl<C: CommandProcessor + Send + Sync + 'static> WidgetService for WidgetServic
         widget_id: String,
         widget_description: String,
     ) -> Result<(), WidgetServiceError> {
-        const MAX_RETRY_COUNT: u32 = 3;
         let mut retry_count = 0;
         loop {
             if retry_count > MAX_RETRY_COUNT {
@@ -159,14 +159,55 @@ impl<C: CommandProcessor + Send + Sync + 'static> WidgetService for WidgetServic
 
 #[cfg(test)]
 mod tests {
+    use kernel::aggregate::WidgetAggregate;
     use kernel::error::AggregateError;
     use kernel::event::WidgetEvent;
     use kernel::processor::{CommandProcessor, MockCommandProcessor};
 
-    use crate::{WidgetService, WidgetServiceError, WidgetServiceImpl};
+    use crate::{WidgetService, WidgetServiceError, WidgetServiceImpl, MAX_RETRY_COUNT};
 
     const WIDGET_NAME: &str = "部品名";
     const WIDGET_DESCRIPTION: &str = "部品の説明";
+
+    #[allow(dead_code)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+    enum DateTime {
+        /// 2023-01-01T00:00:00Z
+        DT2023_01_01_00_00_00_00,
+        /// 2023-01-01T00:00:01Z
+        DT2023_01_01_00_00_00_01,
+        /// 2023-01-01T00:00:02Z
+        DT2023_01_01_00_00_00_02,
+        /// 2023-01-01T00:01:00Z
+        DT2023_01_01_00_00_01_00,
+        /// 2023-01-01T01:00:00Z
+        DT2023_01_01_00_01_00_00,
+        /// 2023-01-01T01:00:00Z
+        DT2023_01_01_01_00_00_00,
+        /// 2023-01-02T00:00:00Z
+        DT2023_01_02_00_00_00_00,
+        /// 2023-02-01T00:00:00Z
+        DT2023_02_01_00_00_00_00,
+        /// 2024-01-01T00:00:00Z
+        DT2024_01_01_00_00_00_00,
+    }
+
+    impl DateTime {
+        fn id(self) -> String {
+            match self {
+                DateTime::DT2023_01_01_00_00_00_00 => "01GNNA1J00PQ9J874NBWERBM3Z",
+                DateTime::DT2023_01_01_00_00_00_01 => "01GNNA1J015CFH0CA590B4K9K6",
+                DateTime::DT2023_01_01_00_00_00_02 => "01GNNA1J02N9H1YCMRA2R9Q562",
+                DateTime::DT2023_01_01_00_00_01_00 => "01GNNA1JZ86A6F1G8HV7NYHDCN",
+                DateTime::DT2023_01_01_00_01_00_00 => "01GNNA3CK0B63HH8HBYQVRJ5Y8",
+                DateTime::DT2023_01_01_01_00_00_00 => "01GNNDFDM0WV3PR6RM8TEA7MZ5",
+                DateTime::DT2023_01_02_00_00_00_00 => "01GNQWE9003DQHKPAAHCDCVTJZ",
+                DateTime::DT2023_02_01_00_00_00_00 => "01GR57SPM0XBGEG4A13ZBW02G2",
+                DateTime::DT2024_01_01_00_00_00_00 => "01HK153X00D14NM09FKYEJ7MPY",
+            }
+            .to_string()
+        }
+    }
 
     /// 部品を作成するテスト
     #[tokio::test]
@@ -247,6 +288,133 @@ mod tests {
             let service = WidgetServiceImpl::new(test.command);
             let result = service
                 .create_widget(test.widget_name, test.widget_description)
+                .await;
+            (test.assert)(test.name, result);
+        }
+    }
+
+    /// 部品名を変更するテスト
+    #[tokio::test]
+    async fn test_change_widget_name() {
+        struct TestCase<T: CommandProcessor> {
+            name: &'static str,
+            widget_id: String,
+            widget_name: String,
+            command: T,
+            assert: fn(name: &str, result: Result<(), WidgetServiceError>),
+        }
+        let tests = vec![
+            TestCase {
+                name: "部品名の形式が正しく永続化時にエラーがない場合は、処理に成功する",
+                widget_id: DateTime::DT2023_01_01_00_00_00_00.id(),
+                widget_name: "部品名v2".to_string(),
+                command: {
+                    let mut command = MockCommandProcessor::new();
+                    command.expect_get_widget_aggregate().returning(|_| {
+                        Box::pin(async {
+                            Ok(WidgetAggregate::new(
+                                DateTime::DT2023_01_01_00_00_00_00.id().parse().unwrap(),
+                            )
+                            .set_name(WIDGET_NAME.to_string())
+                            .set_description(WIDGET_DESCRIPTION.to_string())
+                            .set_version(1))
+                        })
+                    });
+                    command
+                        .expect_update_widget_aggregate()
+                        .withf(|x| {
+                            matches!(
+                                x.events().first().unwrap(),
+                                WidgetEvent::WidgetNameChanged {
+                                    widget_name,
+                                    ..
+                                } if widget_name == "部品名v2"
+                            )
+                        })
+                        .times(1)
+                        .returning(|_| Box::pin(async { Ok(()) }));
+                    command
+                },
+                assert: |name, result| {
+                    assert!(result.is_ok(), "{name}");
+                },
+            },
+            TestCase {
+                name: "部品名の形式が不正な場合、WidgetServiceError::InvalidValue が返る",
+                widget_id: DateTime::DT2023_01_01_00_00_00_00.id().parse().unwrap(),
+                widget_name: String::new(),
+                command: {
+                    let mut command = MockCommandProcessor::new();
+                    command
+                        .expect_get_widget_aggregate()
+                        .returning(|_| Box::pin(async { Ok(WidgetAggregate::default()) }));
+                    command
+                        .expect_update_widget_aggregate()
+                        .returning(|_| Box::pin(async { Ok(()) }));
+                    command
+                },
+                assert: |name, result| {
+                    assert!(
+                        result.is_err_and(|e| matches!(e, WidgetServiceError::InvalidValue)),
+                        "{name}"
+                    );
+                },
+            },
+            TestCase {
+                name: "永続化時に既に集約が更新されている場合、一定数再試行する",
+                widget_id: DateTime::DT2023_01_01_00_00_00_00.id().parse().unwrap(),
+                widget_name: WIDGET_NAME.to_string(),
+                command: {
+                    let mut command = MockCommandProcessor::new();
+                    command.expect_get_widget_aggregate().returning(|_| {
+                        Box::pin(async {
+                            Ok(WidgetAggregate::new(
+                                DateTime::DT2023_01_01_00_00_00_00.id().parse().unwrap(),
+                            )
+                            .set_name(WIDGET_NAME.to_string())
+                            .set_description(WIDGET_DESCRIPTION.to_string())
+                            .set_version(1))
+                        })
+                    });
+                    command
+                        .expect_update_widget_aggregate()
+                        .times((MAX_RETRY_COUNT + 1) as usize) // NOTE: 最初の1回 + 再試行回数
+                        .returning(|_| Box::pin(async { Err(AggregateError::Conflict) }));
+                    command
+                },
+                assert: |name, result| {
+                    assert!(
+                        result.is_err_and(|e| matches!(e, WidgetServiceError::AggregateConfilict)),
+                        "{name}"
+                    );
+                },
+            },
+            TestCase {
+                name: "永続化時に不明なエラーが発生した場合は、WidgetServiceError::Unknow が返る",
+                widget_id: DateTime::DT2023_01_01_00_00_00_00.id().parse().unwrap(),
+                widget_name: WIDGET_NAME.to_string(),
+                command: {
+                    let mut command = MockCommandProcessor::new();
+                    command
+                        .expect_get_widget_aggregate()
+                        .returning(|_| Box::pin(async { Ok(WidgetAggregate::default()) }));
+                    command.expect_update_widget_aggregate().returning(|_| {
+                        Box::pin(async { Err(AggregateError::Unknow("unknown".into())) })
+                    });
+                    command
+                },
+                assert: |name, result| {
+                    assert!(
+                        result.is_err_and(|e| matches!(e, WidgetServiceError::Unknow(_))),
+                        "{name}"
+                    );
+                },
+            },
+        ];
+        for test in tests {
+            let service = WidgetServiceImpl::new(test.command);
+            let result = service
+                .change_widget_name(test.widget_id, test.widget_name)
                 .await;
             (test.assert)(test.name, result);
         }
--- internal/kernel/src/aggregate.rs
+++ internal/kernel/src/aggregate.rs
@@ -87,6 +87,21 @@ impl WidgetAggregate {
         }
         Ok(self)
     }
+
+    pub fn set_name(mut self, name: String) -> Self {
+        self.name = name;
+        self
+    }
+
+    pub fn set_description(mut self, description: String) -> Self {
+        self.description = description;
+        self
+    }
+
+    pub fn set_version(mut self, version: u64) -> Self {
+        self.version = version;
+        self
+    }
 }
 
 /// 集約 (Aggregate) に対するコマンドの処理を成功して保存可能になった状態
