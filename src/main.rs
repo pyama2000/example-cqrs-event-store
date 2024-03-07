258f6b5569a8699126d327cd9647b8ce60096c1f
pyama2000 <takahiko-yamashita@retty.me>
2024-03-08 00:15:09 +0900

    change: 計装を別関数に切り出す

--- src/main.rs
+++ src/main.rs
@@ -3,74 +3,10 @@ use adapter::repository::WidgetRepository;
 use app::WidgetServiceImpl;
 use driver::Server;
 use lib::{application_environment, database_url, opentelemetry_endpoint, Error};
-use opentelemetry_otlp::WithExportConfig;
-use opentelemetry_sdk::trace::{BatchConfig, RandomIdGenerator};
-use opentelemetry_semantic_conventions::resource::{
-    DEPLOYMENT_ENVIRONMENT, SERVICE_NAME, SERVICE_VERSION,
-};
-use opentelemetry_semantic_conventions::SCHEMA_URL;
-use tracing_subscriber::filter::LevelFilter;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let _ = opentelemetry_otlp::new_pipeline()
-        .tracing()
-        .with_trace_config(
-            opentelemetry_sdk::trace::Config::default()
-                .with_id_generator(RandomIdGenerator::default())
-                .with_resource(opentelemetry_sdk::Resource::from_schema_url(
-                    [
-                        opentelemetry::KeyValue::new(SERVICE_NAME, env!("CARGO_PKG_NAME")),
-                        opentelemetry::KeyValue::new(SERVICE_VERSION, env!("CARGO_PKG_VERSION")),
-                        opentelemetry::KeyValue::new(
-                            DEPLOYMENT_ENVIRONMENT,
-                            application_environment(),
-                        ),
-                    ],
-                    SCHEMA_URL,
-                )),
-        )
-        .with_batch_config(BatchConfig::default())
-        .with_exporter(
-            opentelemetry_otlp::new_exporter()
-                .tonic()
-                .with_endpoint(opentelemetry_endpoint()),
-        )
-        .install_batch(opentelemetry_sdk::runtime::Tokio)?;
-    let _ = opentelemetry_otlp::new_pipeline()
-        .logging()
-        .with_log_config(opentelemetry_sdk::logs::Config::default().with_resource(
-            opentelemetry_sdk::Resource::from_schema_url(
-                [
-                    opentelemetry::KeyValue::new(SERVICE_NAME, env!("CARGO_PKG_NAME")),
-                    opentelemetry::KeyValue::new(SERVICE_VERSION, env!("CARGO_PKG_VERSION")),
-                    opentelemetry::KeyValue::new(DEPLOYMENT_ENVIRONMENT, application_environment()),
-                ],
-                SCHEMA_URL,
-            ),
-        ))
-        .with_exporter(
-            opentelemetry_otlp::new_exporter()
-                .tonic()
-                .with_endpoint(opentelemetry_endpoint()),
-        )
-        .install_batch(opentelemetry_sdk::runtime::Tokio)?;
-    tracing_subscriber::registry()
-        .with(
-            EnvFilter::builder()
-                .with_default_directive(LevelFilter::INFO.into())
-                .from_env_lossy(),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .with(
-            opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge::new(
-                &opentelemetry::global::logger_provider(),
-            ),
-        )
-        .init();
+    let _guard = start_instrument()?;
 
     let pool = connect(&database_url()).await?;
     let repository = WidgetRepository::new(pool);
@@ -78,7 +14,75 @@ async fn main() -> Result<(), Error> {
     let server = Server::new("0.0.0.0:8080", service.into());
     server.run().await?;
 
-    opentelemetry::global::shutdown_tracer_provider();
-    opentelemetry::global::shutdown_logger_provider();
     Ok(())
+}
+
+struct OpenTelemetryGuard;
+
+impl Drop for OpenTelemetryGuard {
+    fn drop(&mut self) {
+        opentelemetry::global::shutdown_tracer_provider();
+        opentelemetry::global::shutdown_logger_provider();
+    }
+}
+
+fn start_instrument() -> Result<OpenTelemetryGuard, Error> {
+    use opentelemetry::{global, KeyValue};
+    use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
+    use opentelemetry_otlp::{new_exporter, new_pipeline, WithExportConfig};
+    use opentelemetry_sdk::{
+        logs, runtime,
+        trace::{self, BatchConfig, RandomIdGenerator},
+        Resource,
+    };
+    use opentelemetry_semantic_conventions::{
+        resource::{DEPLOYMENT_ENVIRONMENT, SERVICE_NAME, SERVICE_VERSION},
+        SCHEMA_URL,
+    };
+    use tracing_subscriber::{
+        filter::LevelFilter, fmt, layer::SubscriberExt, registry, util::SubscriberInitExt,
+        EnvFilter,
+    };
+
+    let resource = Resource::from_schema_url(
+        [
+            KeyValue::new(SERVICE_NAME, env!("CARGO_PKG_NAME")),
+            KeyValue::new(SERVICE_VERSION, env!("CARGO_PKG_VERSION")),
+            KeyValue::new(DEPLOYMENT_ENVIRONMENT, application_environment()),
+        ],
+        SCHEMA_URL,
+    );
+    let _ = new_pipeline()
+        .tracing()
+        .with_trace_config(
+            trace::Config::default()
+                .with_id_generator(RandomIdGenerator::default())
+                .with_resource(resource.clone()),
+        )
+        .with_batch_config(BatchConfig::default())
+        .with_exporter(
+            new_exporter()
+                .tonic()
+                .with_endpoint(opentelemetry_endpoint()),
+        )
+        .install_batch(runtime::Tokio)?;
+    let _ = new_pipeline()
+        .logging()
+        .with_log_config(logs::Config::default().with_resource(resource))
+        .with_exporter(
+            new_exporter()
+                .tonic()
+                .with_endpoint(opentelemetry_endpoint()),
+        )
+        .install_batch(runtime::Tokio)?;
+    registry()
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .with(fmt::layer())
+        .with(OpenTelemetryTracingBridge::new(&global::logger_provider()))
+        .init();
+    Ok(OpenTelemetryGuard)
 }
