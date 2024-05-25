88f834676d5974214458c63f8261967d6f4179a2
T.YAMASHITA <33086493+pyama2000@users.noreply.github.com>
2024-05-25 21:27:37 +0900

    feat(infra): AWS をモックして DynamoDB のテーブルを作成する (#16)
    * add: AWS 環境をモックするために Moto Server を立ち上げる
    
    * chore(terraform): モックされた AWS 環境に Terraform でインフラを構築できるようにする
    
    * add: DynamoDB に Aggregate / EventStore テーブルを作成する
    
    * feat: Terraform でリソースを作成する
    
    * chore: apply 時に motoserver が立ち上がるのを待つようにする

---
+++ terraform/providers.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "ap-northeast-1"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    dynamodb = "http://localhost:5000"
+  }
+}
