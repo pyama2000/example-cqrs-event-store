6247303ff24d3040003eeef115331f8e005cdb2b
pyama2000 <takahiko-yamashita@retty.me>
2024-05-25 19:28:57 +0900

    chore(terraform): モックされた AWS 環境に Terraform でインフラを構築できるようにする

---
+++ terraform/providers.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "ap-northeast-1"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+}
