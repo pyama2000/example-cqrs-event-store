dbca2b3ba6e90405fbf1f6a97345a8fe9052dbac
pyama2000 <takahiko-yamashita@retty.me>
2024-07-13 19:06:38 +0900

    add: Command と Event を追加する

--- services/restaurant/internal/kernel/src/lib.rs
+++ services/restaurant/internal/kernel/src/lib.rs
@@ -1,7 +1,13 @@
+#![allow(clippy::module_name_repetitions)]
+
 pub mod aggregate;
+pub mod command;
 pub mod entity;
+pub mod event;
 pub mod id;
 
 pub use aggregate::Aggregate;
+pub use command::Command;
 pub use entity::{Item, ItemCategory, Price, Restaurant};
+pub use event::{Event, EventPayload};
 pub use id::Id;
