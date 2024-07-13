d557804646fd7a699ce3efbfb1ec2d575354c7c9
pyama2000 <takahiko-yamashita@retty.me>
2024-07-13 17:30:56 +0900

    add: Id 構造体を追加する

--- services/restaurant/internal/kernel/src/lib.rs
+++ services/restaurant/internal/kernel/src/lib.rs
@@ -1,15 +1,3 @@
-#[must_use]
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod id;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use id::Id;
