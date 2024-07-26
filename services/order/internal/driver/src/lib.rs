37cbf29f303333200465d812bd4975df7d8ca76b
pyama2000 <takahiko-yamashita@retty.me>
2024-07-26 19:57:19 +0900

    add: Server を定義する

--- services/order/internal/driver/src/lib.rs
+++ services/order/internal/driver/src/lib.rs
@@ -1,15 +1,4 @@
-#[must_use]
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod server;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use server::Server;
+pub(crate) use server::ServiceState;
---
+++ services/order/internal/driver/src/server.rs
@@ -0,0 +1,59 @@
+use std::sync::Arc;
+
+use app::CommandService;
+use axum::Router;
+use tokio::net::{TcpListener, ToSocketAddrs};
+use tokio::signal;
+
+pub(crate) struct ServiceState<C: CommandService> {
+    pub(crate) command: C,
+}
+
+pub struct Server<T: ToSocketAddrs> {
+    addr: T,
+    router: Router,
+}
+
+impl<T: ToSocketAddrs + std::fmt::Display> Server<T> {
+    pub fn new<C>(addr: T, command_service: C) -> Self
+    where
+        C: CommandService + Send + Sync + 'static,
+    {
+        let state = ServiceState {
+            command: command_service,
+        };
+        let router = Router::new().with_state(Arc::new(state));
+        Self { addr, router }
+    }
+
+    /// サーバーを起動する
+    ///
+    /// # Errors
+    ///
+    /// 起動に失敗した際にエラーが発生する
+    pub async fn run(self) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+        let listener = TcpListener::bind(&self.addr).await?;
+        axum::serve(listener, self.router)
+            .with_graceful_shutdown(shutdown_signal())
+            .await?;
+        Ok(())
+    }
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .unwrap_or_else(|e| panic!("failed to install Ctrl+C handler: {e}"));
+    };
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .unwrap_or_else(|e| panic!("failed to install signal handler: {e}"))
+            .recv()
+            .await;
+    };
+    tokio::select! {
+        () = ctrl_c => println!("receive ctrl_c signal"),
+        () = terminate => println!("receive terminate"),
+    }
+}
