ca9e34727fe9c943acf2a256fbe03e5cf733b311
pyama2000 <takahiko-yamashita@retty.me>
2024-11-22 16:51:20 +0900

    add: テナントサービスを追加する

---
+++ proto/generate/rust/src/lib.rs
@@ -0,0 +1,9 @@
+// @generated
+pub mod tenant {
+    #[cfg(feature = "tenant-v1")]
+    // @@protoc_insertion_point(attribute:tenant.v1)
+    pub mod v1 {
+        include!("tenant.v1.rs");
+        // @@protoc_insertion_point(tenant.v1)
+    }
+}
