3ef68d7900ab01d5232310d4b066eca94112aa4e
pyama2000 <takahiko-yamashita@retty.me>
2025-02-26 17:12:03 +0900

    add: テナントごとのアイテムのマップをフィールドに定義する

--- services/cart/internal/kernel/src/command/model/aggregate.rs
+++ services/cart/internal/kernel/src/command/model/aggregate.rs
@@ -1,22 +1,40 @@
+use std::collections::HashMap;
+
 use crate::id::Id;
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
+use super::entity::{Item, Tenant};
+
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
 pub struct Aggregate {
     id: Id<Aggregate>,
+    item_ids_by_tenant_id: HashMap<Id<Tenant>, Vec<Id<Item>>>,
     /// 集約のバージョン
     version: u128,
 }
 
 impl Aggregate {
     #[must_use]
-    pub fn new(id: Id<Aggregate>, version: u128) -> Self {
-        Self { id, version }
+    pub fn new(
+        id: Id<Aggregate>,
+        item_ids_by_tenant_id: HashMap<Id<Tenant>, Vec<Id<Item>>>,
+        version: u128,
+    ) -> Self {
+        Self {
+            id,
+            item_ids_by_tenant_id,
+            version,
+        }
     }
 
     /// 集約のID
     #[must_use]
     pub fn id(&self) -> &Id<Aggregate> {
         &self.id
+    }
+
+    #[must_use]
+    pub fn item_ids_by_tenant_id(&self) -> &HashMap<Id<Tenant>, Vec<Id<Item>>> {
+        &self.item_ids_by_tenant_id
     }
 
     /// 集約のバージョン
