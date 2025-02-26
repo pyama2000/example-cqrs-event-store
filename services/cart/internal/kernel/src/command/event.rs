16050425c4814abeefb3e1a2e0a6db9ecd2d815b
pyama2000 <takahiko-yamashita@retty.me>
2025-02-26 17:10:54 +0900

    feat: コマンドとイベントを定義する

--- services/cart/internal/kernel/src/command/event.rs
+++ services/cart/internal/kernel/src/command/event.rs
@@ -1,3 +1,17 @@
+use crate::id::Id;
+
+use super::model::entity::{Item, Tenant};
+
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Event {
+    Created,
+    ItemAdded {
+        tenant_id: Id<Tenant>,
+        item_id: Id<Item>,
+    },
+    ItemRemoved {
+        tenant_id: Id<Tenant>,
+        item_id: Id<Item>,
+    },
+    OrderPlaced,
 }
--- services/cart/internal/kernel/src/command/model/entity.rs
+++ services/cart/internal/kernel/src/command/model/entity.rs
@@ -1,1 +1,5 @@
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Default, Hash)]
+pub struct Tenant;
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Default, Hash)]
+pub struct Item;
