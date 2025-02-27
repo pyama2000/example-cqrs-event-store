68486f4432fa19caa236ae2eb07247cae5336524
pyama2000 <takahiko-yamashita@retty.me>
2025-02-27 11:25:36 +0900

    feat: UseCaseを実装する

--- services/cart/internal/app/src/command/error.rs
+++ services/cart/internal/app/src/command/error.rs
@@ -1,4 +1,7 @@
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum CommandUseCaseError {}
+pub enum CommandUseCaseError {
+    #[error("Aggregate not found")]
+    AggregateNotFound,
+}
--- services/cart/internal/app/src/command/usecase.rs
+++ services/cart/internal/app/src/command/usecase.rs
@@ -1,7 +1,42 @@
+use std::future::Future;
+
+use kernel::command::command::Command;
+use kernel::command::model::aggregate::{Aggregate, ApplyCommand as _};
+use kernel::command::model::entity::{Item, Tenant};
 use kernel::command::processor::CommandProcessor;
+use kernel::id::Id;
+
+use super::error::CommandUseCaseError;
 
 /// ユースケースのインターフェイス
-pub trait CommandUseCaseExt {}
+pub trait CommandUseCaseExt {
+    /// カートを作成する
+    fn create(
+        &self,
+    ) -> impl Future<Output = Result<Result<Id<Aggregate>, CommandUseCaseError>, anyhow::Error>> + Send;
+
+    /// カートに商品を追加する
+    fn add_item(
+        &self,
+        id: Id<Aggregate>,
+        tenant_id: Id<Tenant>,
+        item_id: Id<Item>,
+    ) -> impl Future<Output = Result<Result<(), CommandUseCaseError>, anyhow::Error>> + Send;
+
+    /// カートの商品を削除する
+    fn remove_item(
+        &self,
+        id: Id<Aggregate>,
+        tenant_id: Id<Tenant>,
+        item_id: Id<Item>,
+    ) -> impl Future<Output = Result<Result<(), CommandUseCaseError>, anyhow::Error>> + Send;
+
+    /// カートの商品を注文する
+    fn place_order(
+        &self,
+        id: Id<Aggregate>,
+    ) -> impl Future<Output = Result<Result<(), CommandUseCaseError>, anyhow::Error>> + Send;
+}
 
 /// ユースケースの実態
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
@@ -15,4 +50,66 @@ impl<P: CommandProcessor> CommandUseCase<P> {
     }
 }
 
-impl<P> CommandUseCaseExt for CommandUseCase<P> where P: CommandProcessor + Send + Sync + 'static {}
+impl<P> CommandUseCaseExt for CommandUseCase<P>
+where
+    P: CommandProcessor + Send + Sync + 'static,
+{
+    async fn create(&self) -> Result<Result<Id<Aggregate>, CommandUseCaseError>, anyhow::Error> {
+        use anyhow::Context as _;
+
+        let mut aggregate = Aggregate::default();
+        let id = aggregate.id().clone();
+        let event = aggregate
+            .apply_command(Command::Create)??
+            .pop()
+            .with_context(|| "event not present")?;
+        self.processor.create(aggregate, event).await??;
+        Ok(Ok(id))
+    }
+
+    async fn add_item(
+        &self,
+        id: Id<Aggregate>,
+        tenant_id: Id<Tenant>,
+        item_id: Id<Item>,
+    ) -> Result<Result<(), CommandUseCaseError>, anyhow::Error> {
+        let Some(mut aggregate): Option<Aggregate> = self.processor.get(id).await?? else {
+            return Ok(Err(CommandUseCaseError::AggregateNotFound));
+        };
+        let events = aggregate.apply_command(Command::AddItem { tenant_id, item_id })??;
+        self.processor.update(aggregate, events).await??;
+        Ok(Ok(()))
+    }
+
+    async fn remove_item(
+        &self,
+        id: Id<Aggregate>,
+        tenant_id: Id<Tenant>,
+        item_id: Id<Item>,
+    ) -> Result<Result<(), CommandUseCaseError>, anyhow::Error> {
+        let Some(mut aggregate): Option<Aggregate> = self.processor.get(id).await?? else {
+            return Ok(Err(CommandUseCaseError::AggregateNotFound));
+        };
+        let events = match aggregate.apply_command(Command::RemoveItem { tenant_id, item_id })? {
+            Ok(events) => events,
+            Err(e) => match e {
+                kernel::command::error::CommandKernelError::ItemNotFound => return Ok(Ok(())),
+                _ => return Err(e.into()),
+            },
+        };
+        self.processor.update(aggregate, events).await??;
+        Ok(Ok(()))
+    }
+
+    async fn place_order(
+        &self,
+        id: Id<Aggregate>,
+    ) -> Result<Result<(), CommandUseCaseError>, anyhow::Error> {
+        let Some(mut aggregate): Option<Aggregate> = self.processor.get(id).await?? else {
+            return Ok(Err(CommandUseCaseError::AggregateNotFound));
+        };
+        let events = aggregate.apply_command(Command::PlaceOrder)??;
+        self.processor.update(aggregate, events).await??;
+        Ok(Ok(()))
+    }
+}
