bcd9df85cf74c3d2f27782b83eb10ab9c4d8a9bc
pyama2000 <takahiko-yamashita@retty.me>
2025-03-04 19:19:05 +0900

    feat: gRPCサーバーを起動する

--- services/cart/src/main.rs
+++ services/cart/src/main.rs
@@ -1,5 +1,26 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    println!("Hello, world!");
+    let addr = format!(
+        "[::1]:{}",
+        std::env::var("PORT").map_err(|e| format!("PORT must be set: {e:?}"))?
+    );
+    let config = aws_config::defaults(aws_config::BehaviorVersion::v2024_03_28())
+        .endpoint_url(format!(
+            "http://localhost:{}",
+            std::env::var("LOCALSTACK_GATEWAY_PORT").unwrap_or("4566".to_string())
+        ))
+        .test_credentials()
+        .load()
+        .await;
+    let dynamodb = adapter::command::persistence::dynamodb(&config);
+    let server = driver::server::Server::new(driver::server::Service::new(
+        app::command::usecase::CommandUseCase::new(
+            adapter::command::repository::CommandRepository::new(dynamodb.clone()),
+        ),
+        app::query::usecase::QueryUseCase::new(adapter::query::repository::QueryRepository::new(
+            dynamodb,
+        )),
+    ));
+    server.run(addr.parse()?).await?;
     Ok(())
 }
