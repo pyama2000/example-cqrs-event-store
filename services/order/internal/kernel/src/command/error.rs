dbfeed9103ed4fc76dc7db9fd2cf8cbb6a479cee
pyama2000 <takahiko-yamashita@retty.me>
2025-03-06 11:56:09 +0900

    feat: 集約にコマンドを実行する関数を実装する

--- services/order/internal/kernel/src/command/error.rs
+++ services/order/internal/kernel/src/command/error.rs
@@ -1,4 +1,17 @@
 use thiserror::Error;
 
+use super::model::entity::OrderStatus;
+
 #[derive(Debug, Error, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum CommandKernelError {}
+pub enum CommandKernelError {
+    #[error("Aggregate not created")]
+    AggregateNotCreated,
+    #[error("Aggregate already created")]
+    AggregateAlreadyCreated,
+    #[error("Cannot update Aggregate version")]
+    AggregateVersionOverflowed,
+    #[error("Items is empty")]
+    ItemsIsEmpty,
+    #[error("Invalid operation: current_status =  {current_status:?}")]
+    InvalidOperation { current_status: OrderStatus },
+}
