aa48654220207cbbd786d7305e77a94cd1a8e121
pyama2000 <takahiko-yamashita@retty.me>
2025-04-02 21:50:39 +0900

    add: 各サービスのリソースをTerraformで作成する

---
+++ terraform/terraform.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "1.10.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.58.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.4.2"
+    }
+  }
+}
